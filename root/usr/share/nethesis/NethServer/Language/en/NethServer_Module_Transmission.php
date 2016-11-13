2da1e352322a4794b312890b00b2cbc29d298c96
Stephane de Labrusse <stephdl@de-labrusse.fr>
2016-11-13 22:35:57 +0100

    ns7 work7

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Transmission.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Transmission.php
@@ -1,6 +1,6 @@
 <?php
 $L['Transmission_header'] = 'Transmission Access Settings';
-$L['ShellInaBox_Title'] = 'Transmission';
+$L['Transmission_Title'] = 'Transmission';
 $L['status_label'] = 'Enable Transmission';
 $L['Webaccess_private_label'] = 'Private Access';
 $L['Webaccess_public_label'] = 'Public Access';
